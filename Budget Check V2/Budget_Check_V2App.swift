454da2ac81218e39258b356d8e963918351b2216
KyleGal <107329064+KyleGal@users.noreply.github.com>
2023-07-07 11:20:38 -0700

    Initial Commit

---
+++ Budget Check V2/Budget_Check_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_Check_V2App.swift
+//  Budget Check V2
+//
+//  Created by Kyle Gabriel Galvez on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Budget_Check_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budget Check V2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Budget Check V2
+//
+//  Created by Kyle Gabriel Galvez on 7/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
